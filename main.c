967e6eddba1ef03c1d8549a8133309e10dcd86b6
André Siviero <altsiviero@gmail.com>
2011-11-16 17:26:44 -0200

    16/11 17:26

--- main.c
+++ main.c
@@ -13,8 +13,8 @@
 #include <time.h>
 #define MAX_PASSENGERS 15
 
-sem_t mutex,boat,cross_start,cross_end,mutex2,in;
-int count=0,inside=0,fatboy;
+sem_t mutex,boat,cross_start,cross_end,mutex2,in,mutex3;
+int count=0,inside=0,fatboy,if_fuck=0;
 
 void *passenger(void *id) {
 	int tid = *(int *)id;
@@ -32,9 +32,21 @@ void *passenger(void *id) {
 		sem_post(&mutex);
 	sem_wait(&cross_end);
 		sleep(1);
+		printf("Jovem %d atravesssando matagal!\n",tid);
+		if (if_fuck){
+			printf("Opa! Os segurancas pegaram um ... nao vou poder entrar no show... o jeito vai ser  o telao mesmo!\n");
+			sem_post(&mutex2);
+			pthread_exit(NULL);
+		}
 		sem_wait(&in);
-			inside++;
-			if(tid == fatboy) {printf("GORDO SO FAZ GORDICE\n"); exit(1);}
+			if(tid == fatboy) {
+				printf("Jovem %d preso na cerca... Shii!! Os segurancas me pegaram!\n",tid);
+				sem_wait(&mutex3);
+					if_fuck = 1;
+				sem_post(&mutex3);
+				sem_post(&mutex2);
+				//exit(1);
+			}else inside++;
 			if(inside==3) {
 				inside = 0;
 				sem_post(&mutex2);
@@ -53,10 +65,21 @@ void *boatman() {
 		for(int i=0;i<3;i++) sem_post(&cross_end);
 		printf("Barqueiro espera todos atravessarem o matagal...\n");
 		sem_wait(&mutex2);
+		sem_wait(&mutex3);
+		printf("IF FUCK: %d\n",if_fuck);
+		if (if_fuck){
+			printf("Opa! Problemas! Os segurancas pegaram um! Acabou meu esquema...\n");
+
+			//TODO: Codigo do named pipe!
+
+			exit(1);
+		}
+		sem_post(&mutex3);
 		sleep(2);
 		sem_wait(&mutex);
 			count-=3;
 		sem_post(&mutex);
+		printf("Tudo certo! Barqueiro retorna para fazer outra viagem...\n");
 		for(int i=0;i<3;i++) sem_post(&boat);
 	}
 	//return;
@@ -64,17 +87,23 @@ void *boatman() {
 
 
 int main() {
+
+
 	sem_init(&mutex,0,1);
-	sem_init(&mutex2,0,1);
+	sem_init(&mutex2,0,0);
 	sem_init(&cross_start,0,0);
 	sem_init(&cross_end,0,0);
 	sem_init(&boat,0,3);
 	sem_init(&in,0,1);
+	sem_init(&mutex3,0,1);
+
+
 	srand(time(NULL));
 	fatboy = rand()%15;
 	printf("OLHA O GORDO: %d\n",fatboy);
-	sleep(5);
+	sleep(2);
 	long *taskid[MAX_PASSENGERS];
+
 	for(int i=0;i<MAX_PASSENGERS;i++) {
 		taskid[i] = (long *) malloc(sizeof(long));
 		*taskid[i] = i;
@@ -90,5 +119,7 @@ int main() {
 		pthread_join(passengers[i],NULL);
 	}
 	pthread_join(carrier,NULL);
+
+	return 0;
 }
 
